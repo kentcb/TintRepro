500b6687371944d9d390a55f6a6bf5c32c326b92
Kent Boogaart <kent.boogaart@gmail.com>
2017-02-22 15:26:54 +1030

    Initial commit

---
+++ TintRepro/TintRepro/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TintRepro
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.Update();
+
+            this.button.Clicked += (s, e) =>
+              {
+                  this.Update();
+              };
+        }
+
+        private void Update()
+        {
+            tint.A = int.Parse(alpha.Text);
+            tint.R = int.Parse(red.Text);
+            tint.G = int.Parse(green.Text);
+            tint.B = int.Parse(blue.Text);
+            this.tintedImage.ReloadImage();
+        }
+    }
+}
